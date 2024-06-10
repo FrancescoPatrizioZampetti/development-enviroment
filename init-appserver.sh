3df89411299d49ded81bdcf32d8f03f22dc96363
FrancescoPatrizioZampetti <zampetti1@hotmail.com>
2024-06-10 22:26:39 +0200

    up

---
+++ init-appserver.sh
@@ -0,0 +1,5 @@
+useradd devops
+echo 'devops:appserver123' | chpasswd devops
+systemctl stop NetworkManager
+systemctl disable NetworkManager            
+ip addr add 10.0.0.2 dev enp0s3
--- init-jumpserver.sh
+++ init-jumpserver.sh
@@ -1,2 +1,5 @@
+# creo utente
 useradd jump-host
 echo 'jump-host:jumpserver123' | chpasswd jump-host
+# assegno ip statico
+ip addr add 192.167.1.18 dev enp0s3
